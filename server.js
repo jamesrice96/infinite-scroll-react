9d667b07c4a9d8e80d3ad1868219cb76e2a4f3ad
James Rice <james@purplecs.com>
2019-05-13 14:26:45 +0100

    getting ready for deployment - loading from client if in prod

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const config = require('universal-config');
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
 const express = require('express');
+const path = require('path');
 
 const unsplash = new Unsplash({
   applicationId: config.get('APPLICATION_ID'),
@@ -16,6 +17,16 @@ const unsplash = new Unsplash({
 
 const app = express();
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  //Load client index file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.get('/api/photos', async (req, res) => {
   await unsplash.photos
     .listPhotos(req.query.start, req.query.count)
@@ -23,6 +34,8 @@ app.get('/api/photos', async (req, res) => {
     .then(json => res.json(json));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
